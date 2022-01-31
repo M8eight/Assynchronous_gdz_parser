1853cf869add53cc619404fa93b3c955226f8162
m8eight <alekskoch234@gmail.com>
2022-01-31 19:59:56 +0300

    2.0.0 add comments, loading state, warning, image smaller size

--- app.js
+++ app.js
@@ -1,59 +1,39 @@
-const express = require('express');
-const app = express();
-const axios = require('axios');
-const {
-    parse
-} = require('node-html-parser');
+/*!
+ *Assynchronous_gdz_parser
+ *By: m8eight
+ *MIT Licensed
+*/
 
+//init crawler module
+let parser = require('./lib/parser.js');
+const { response } = require('express');
+const express = require('express'); //framework
+const app = express(); 
+
+
+//import method for processing json from express
 const jsonParser = express.json();
 
-app.post('/post', jsonParser, function (req, res) {
-    if (!req.body) return res.sendStatus(303);
+app.use(express.static('public'));
 
-    parser();
+app.post('/post', jsonParser, function (request, response) {
+    //if in request is empty then send status 400 (Bad request)
+    if (!request.body) return response.json({'error': 'No request'});
 
-    async function parser() {
-        var url = '';
-        var number = req.body.number;
+    //pick up in request all property, property in form
+    let number = request.body.number;
+    let book = request.body.book;
 
-        switch (req.body.book) {
-            case 'alg9':
-                url = `https://gdz.ru/class-9/algebra/dorofeev/${number}-nom/`
-                break;
-
-            default:
-                res.sendStatus(404);
-                break;
-        }
-
-        await axios.get(url)
-            .then(async (response) => {
-                const root = parse(response.data);
-                var rows = {};
-                var rowsRend = [];
-                rows = (root.querySelectorAll('.with-overtask > img'));
-
-                await Promise.all(rows.map(async row => {
-
-                    let href = 'https:' + row.getAttribute('src');
-                    rowsRend.push(href);
-
-                }));
-
-                res.json(rowsRend);
-            })
-            .catch(function (error) {
-                res.json(['error']);
-            });
-
-        //console.log(rowsRend);
-
-    }
-
+    //call crawler module
+    parser.requestHandler(number, book, response);
 });
 
+//index page, send view
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000);
+//start express server
+app.listen(3000, '127.0.0.1', function () {
+    console.info('server has started to 127.0.0.1:3000');
+});
---
+++ public/ajaxPostRequest.js
@@ -0,0 +1,50 @@
+$("#submit").click(function (button) {
+    //take div in which we will put pictures and info and warning
+    var imgContainer = $('#imgContainer');
+    //reset sent property button
+    button.preventDefault();
+    //if there is any data in the container, then we clear it
+    if (document.getElementsByClassName('imgStyle').length != 0) {
+        $('#imgContainer').empty();
+    }
+    let bookParam = $('select[name=book]').val();
+    let numberParam = $('input[name=number]').val();
+    let jsonReqData = JSON.stringify({
+        'book': bookParam,
+        'number': numberParam
+    });
+
+    //replace send button to 'loading' button
+    {
+        $('#submit').text(' Loading');
+        $('#submit').addClass('disabled');
+        $('#submit').append(' <span class="spinner-grow spinner-grow-sm text-success" role="status " aria-hidden="true"></span>');    
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/post",
+        data: jsonReqData,
+        dataType: "json",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (response) {
+        //return send button
+        $('#submit').empty();
+        $('#submit').text('Send');
+        $('#submit').removeClass('disabled');
+
+        if (response.hasOwnProperty('error')) {
+            // $('#imgContainer').append($('#imgContainer').innerHTML = '<h3 class="text-danger">Error, see cause ↓</h3>');
+            $('#imgContainer').append($('#imgContainer').innerHTML = `<div class="alert alert-warning" role="alert"><h3 class="text-danger">Error</h3>
+            Cause: ${response['error']}</div>`);
+        } else {
+            for (let row in response) {
+                $('#imgContainer').append(`<img width=700px class="imgStyle img-fluid" src="${response[row]}"/>`);
+            }
+        }
+
+    });
+
+});
