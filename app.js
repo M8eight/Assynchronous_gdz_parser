c62e2e44c585e31968f622c29730eca4453e9937
m8eight <alekskoch234@gmail.com>
2022-02-15 00:42:48 +0300

    add eslint configuration and update files

--- app.js
+++ app.js
@@ -5,35 +5,34 @@
 */
 
 //init crawler module
-let parser = require('./lib/parser.js');
-const { response } = require('node-html-parser');
-const express = require('express'); //framework
+let parser = require("./lib/parser.js");
+const express = require("express"); //framework
 const app = express(); 
 
 
 //import method for processing json from express
 const jsonParser = express.json();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.post('/post', jsonParser, function (request, response) {
-    //if in request is empty then send status 400 (Bad request)
-    if (!request.body) return response.json({'error': 'No request'});
+app.post("/post", jsonParser, function (request, response) {
+	//if in request is empty then send status 400 (Bad request)
+	if (!request.body) return response.json({"error": "No request"});
 
-    //pick up in request all property, property in form
-    let number = request.body.number;
-    let book = request.body.book;
+	//pick up in request all property, property in form
+	let number = request.body.number;
+	let book = request.body.book;
 
-    //call crawler module
-    parser.requestHandler(number, book, response);
+	//call crawler module
+	parser.requestHandler(number, book, response);
 });
 
 //index page, send view
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/public/index.html");
 });
 
 //start express server
-app.listen(3000, '127.0.0.1', function () {
-    console.info('server has started to 127.0.0.1:3000');
+app.listen(3000, "127.0.0.1", function () {
+	console.info("server has started to 127.0.0.1:3000");
 });
--- public/ajaxPostRequest.js
+++ public/ajaxPostRequest.js
@@ -1,53 +1,53 @@
 $("#submit").click(function (button) {
-    //reset sent property button
-    button.preventDefault();
-    //if there is any data in the container, then we clear it
-    if (document.getElementsByClassName('imgStyle').length != 0) {
-        $('#imgContainer').empty();
-    }
-    let bookParam = $('select[name=book]').val();
-    let numberParam = $('input[name=number]').val();
-    let jsonReqData = JSON.stringify({
-        'book': bookParam,
-        'number': numberParam
-    });
-    //replace send button to 'loading' button
-    {
-        $('#submit').text(' Loading');
-        $('#submit').addClass('disabled');
-        $('#submit').append(' <span class="spinner-grow spinner-grow-sm text-success" role="status " aria-hidden="true"></span>');    
-    }
+	//reset sent property button
+	button.preventDefault();
+	//if there is any data in the container, then we clear it
+	if (document.getElementsByClassName("imgStyle").length != 0) {
+		$("#imgContainer").empty();
+	}
+	let bookParam = $("select[name=book]").val();
+	let numberParam = $("input[name=number]").val();
+	let jsonReqData = JSON.stringify({
+		"book": bookParam,
+		"number": numberParam
+	});
+	//replace send button to 'loading' button
+	{
+		$("#submit").text(" Loading");
+		$("#submit").addClass("disabled");
+		$("#submit").append(" <span class=\"spinner-grow spinner-grow-sm text-success\" role=\"status \" aria-hidden=\"true\"></span>");    
+	}
 
 
-    $.ajax({
-        type: "POST",
-        url: "/post",
-        data: jsonReqData,
-        dataType: "json",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).done(function (response) {
+	$.ajax({
+		type: "POST",
+		url: "/post",
+		data: jsonReqData,
+		dataType: "json",
+		headers: {
+			"Content-Type": "application/json"
+		}
+	}).done(function (response) {
 
-        //return send button
-        $('#submit').empty();
-        $('#submit').text('Send');
-        $('#submit').removeClass('disabled');
+		//return send button
+		$("#submit").empty();
+		$("#submit").text("Send");
+		$("#submit").removeClass("disabled");
 
-        if ($('#imgContainer').has('div')) {
-            $('#imgContainer').empty();
-        }
+		if ($("#imgContainer").has("div")) {
+			$("#imgContainer").empty();
+		}
 
-        if (response.hasOwnProperty('error')) {
-            // $('#imgContainer').append($('#imgContainer').innerHTML = '<h3 class="text-danger">Error, see cause ↓</h3>');
-            $('#imgContainer').append($('#imgContainer').innerHTML = `<div class="alert alert-warning" role="alert"><h3 class="text-danger">Error</h3>
-            Cause: ${response['error']}</div>`);
-        } else {
-            for (let row in response) {
-                $('#imgContainer').append(`<img width=700px class="imgStyle img-fluid" src="${response[row]}"/>`);
-            }
-        }
+		if (response.hasOwnProperty("error")) {
+			// $('#imgContainer').append($('#imgContainer').innerHTML = '<h3 class="text-danger">Error, see cause ↓</h3>');
+			$("#imgContainer").append($("#imgContainer").innerHTML = `<div class="alert alert-warning" role="alert"><h3 class="text-danger">Error</h3>
+            Cause: ${response["error"]}</div>`);
+		} else {
+			for (let row in response) {
+				$("#imgContainer").append(`<img width=700px class="imgStyle img-fluid" src="${response[row]}"/>`);
+			}
+		}
 
-    });
+	});
 
 });
