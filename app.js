7da8b824796e16a316b135100991aba44a8c468a
m8eight <alekskoch234@gmail.com>
2022-03-06 20:50:12 +0300

    3.0.0 add arrows to select a page, new dempencies "chalk"

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
 /*!
- *Assynchronous_gdz_parser
+ *Asynchronous_gdz_parser
  *By: m8eight
  *MIT Licensed
-*/
+ */
 
-//init crawler module
-let parser = require("./lib/requestHandler.js");
+const chalk = require("chalk");
+
 const express = require("express"); //framework
-const app = express(); 
+const app = express();
 
-
-//import method for processing json from express
 const jsonParser = express.json();
-
 app.use(express.static("public"));
 
-app.post("/post", jsonParser, function (request, response) {
-	//if in request is empty then send status 400 (Bad request)
-	if (!request.body) return response.json({"error": "No request"});
+let parserHandler = require("./lib/handlers/mainParser.js");
+let checkUrlHandler = require("./lib/handlers/checkUrl.js");
 
-	//pick up in request all property, property in form
-	let number = request.body.number;
-	let book = request.body.book;
-
-	//call crawler module
-	parser.requestHandler(number, book, response);
-});
-
-//index page, send view
 app.get("/", function (req, res) {
-	res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
-//start express server
+app.post("/post", jsonParser, function (request, response) {
+    if (!request.body) return response.json({ error: "No request" });
+
+    let number = request.body.number;
+    let book = request.body.book;
+
+    parserHandler.requestHandler(number, book, response);
+});
+
+app.post("/checkUrl", jsonParser, function (request, response) {
+    if (!request.body) return response.json({ error: "No request" });
+
+    let number = request.body.number;
+    let book = request.body.book;
+
+    checkUrlHandler.checkUrl(number, book, response);
+});
+
 app.listen(3000, "127.0.0.1", function () {
-	console.info("server has started to 127.0.0.1:3000");
+    console.info(chalk.yellow("Server has started to 127.0.0.1:3000"));
 });
--- public/ajaxPostRequest.js
+++ public/ajaxPostRequest.js
@@ -1,53 +1,162 @@
+var arrow = {};
+
 $("#submit").click(function (button) {
-	//reset sent property button
-	button.preventDefault();
-	//if there is any data in the container, then we clear it
-	if (document.getElementsByClassName("imgStyle").length != 0) {
-		$("#imgContainer").empty();
-	}
-	let bookParam = $("select[name=book]").val();
-	let numberParam = $("input[name=number]").val();
-	let jsonReqData = JSON.stringify({
-		"book": bookParam,
-		"number": numberParam
-	});
-	//replace send button to 'loading' button
-	{
-		$("#submit").text(" Loading");
-		$("#submit").addClass("disabled");
-		$("#submit").append(" <span class=\"spinner-grow spinner-grow-sm text-success\" role=\"status \" aria-hidden=\"true\"></span>");    
-	}
-
-
-	$.ajax({
-		type: "POST",
-		url: "/post",
-		data: jsonReqData,
-		dataType: "json",
-		headers: {
-			"Content-Type": "application/json"
-		}
-	}).done(function (response) {
-
-		//return send button
-		$("#submit").empty();
-		$("#submit").text("Send");
-		$("#submit").removeClass("disabled");
-
-		if ($("#imgContainer").has("div")) {
-			$("#imgContainer").empty();
-		}
-
-		if (response.hasOwnProperty("error")) {
-			// $('#imgContainer').append($('#imgContainer').innerHTML = '<h3 class="text-danger">Error, see cause ↓</h3>');
-			$("#imgContainer").append($("#imgContainer").innerHTML = `<div class="alert alert-warning" role="alert"><h3 class="text-danger">Error</h3>
-            Cause: ${response["error"]}</div>`);
-		} else {
-			for (let row in response) {
-				$("#imgContainer").append(`<img width=700px class="imgStyle img-fluid" src="${response[row]}"/>`);
-			}
-		}
-
-	});
-
+    button.preventDefault();
+    loadingBlock();
+    var book = $("select[name=book]").val();
+    var number = parseInt($("input[name=number]").val());
+    arrow.number = number;
+    main(book, number);
 });
+
+async function main(book, number) {
+    let arrowNumber = number.toString();
+
+    arrow = {
+        book: book,
+        number: arrowNumber,
+    };
+
+    sendRequest(book, number);
+
+    await fetch("/checkUrl", {
+        method: "POST",
+        body: JSON.stringify({
+            book: arrow.book,
+            number: arrow.number,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(function (data) {
+            data.json().then((jsonResponse) => {
+                if (jsonResponse.status == true) {
+                    if ($("#leftArrowButton").val() == undefined) {
+                        $("#leftArrow").append(`
+                            <button id="leftArrowButton" type="button" class="btn btn-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                                </svg>
+                            </button>
+                        `);
+                        $("#leftArrow").click(function () {
+                            $("#leftArrowButton").attr("disabled", "");
+                            $("#leftArrowButton").off();
+                            let formNumber = parseInt($("input[name=number]").val());
+                            $("input[name=number]").val(--arrow.number);
+                            main(arrow.book, arrow.number).finally(() => {
+                                $("#leftArrowButton").removeAttr("disabled");
+                            });
+                        });
+                    }
+                }
+            });
+        })
+        .catch((err) => {
+            $("#leftArrow").html(`
+                            <button disabled id="leftArrowButton" type="button" class="btn btn-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                                </svg>
+                            </button>
+                        `);
+        });
+
+    await fetch("/checkUrl", {
+        method: "POST",
+        body: JSON.stringify({
+            book: arrow.book,
+            number: arrow.number,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(function (data) {
+            data.json().then((jsonResponse) => {
+                if (jsonResponse.status == true) {
+                    if ($("#rightArrowButton").val() == undefined) {
+                        $("#rightArrow").append(`
+                        <button id="rightArrowButton" type="button" class="btn btn-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                            </svg>
+                        </button>
+                         `);
+                        $("#rightArrow").click(function () {
+                            $("#rightArrowButton").attr("disabled", "");
+                            $("#rightArrowButton").off();
+                            $("input[name=number]").val(++arrow.number);
+                            main(arrow.book, arrow.number).finally(() => {
+                                $("#rightArrowButton").removeAttr("disabled");
+                            });
+                        });
+                    }
+                }
+            });
+        })
+        .catch((err) => {
+            $("#rightArrow").append(`
+            <button disabled id="rightArrowButton" type="button" class="btn btn-primary">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"></path>
+                </svg>
+            </button>
+             `);
+        });
+}
+
+function sendAlert(errText) {
+    $("#imgContainer").append(
+        ($("#imgContainer").innerHTML = `
+            <div class="alert alert-warning" role="alert"><h3 class="text-danger">Error</h3>
+            Cause: ${errText}</div>
+        `)
+    );
+}
+
+function loadingBlock() {
+    if ($(".imgStyle").length != 0) {
+        $("#imgContainer").empty();
+    }
+
+    let submitButton = $("#submit");
+    submitButton.text(" ..Loading");
+    submitButton.addClass("disabled");
+    submitButton.append(`
+          <span class="spinner-grow spinner-grow-sm text-success role="status " aria-hidden="true"></span>
+    `);
+}
+
+function sendRequest(book, number) {
+    let jsonRequest = JSON.stringify({ book: book, number: number });
+
+    $.ajax({
+        type: "POST",
+        url: "/post",
+        data: jsonRequest,
+        dataType: "json",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).done(function (response) {
+        if ($("#imgContainer").has("div")) {
+            $("#imgContainer").empty();
+        }
+
+        let submitButton = $("#submit");
+        submitButton.text("Send");
+        submitButton.removeClass("disabled");
+
+        if (response.hasOwnProperty("error")) {
+            sendAlert(response["error"]);
+        } else {
+            for (let row in response) {
+                $("#imgContainer").append(
+                    `<img width=700px class="imgStyle img-fluid" src="${response[row]}"/>`
+                );
+            }
+        }
+    });
+}
